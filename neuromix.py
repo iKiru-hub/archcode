cb1d78cd37acbce880b8ebd7cd4f709d481b7fea
Krubeal <dkirubel@gmail.com>
2022-11-18 23:00:18 +0100

    start translating the code

---
+++ neuromix.py
@@ -0,0 +1,80 @@
+
+""" brain.py """
+
+utils = {'generate_substrate': 0}
+
+substrate = {'init': [],
+             'step': [],
+             'update': [],
+             'check_dna': [],
+             'initialize': [],
+             'collect_input': 0,
+             'add_loss': [],
+             'get_loss': 0,
+             'get_output': 0,
+             ''}
+
+
+
+""" sim.py """
+
+testmap = {'init': 0,
+           'add_input': [],
+           'add_target': [],
+           'rigenerate_data': [],
+           'plot_stimuli': [],
+           'plot_raster': [],
+           'testing': [],
+           }
+
+gym = {'init': 0,
+       'add_substrate': [],
+       'simulation': [],
+       'long_simulation': [],
+       'test_substrate': [],
+       'plot_classes_test': [],
+       'get_input': [],
+       'get_target': []}
+
+agentevo = {'init': 0,
+            'step': [],
+            'collect_input': [],
+            'get_output': [],
+            'get_fitness': [],
+            'get_substrate': [],
+            'get_info': 0,
+            'get_name': 0,
+            'set_fitness': 0,
+            'reset': 0}
+
+dna_generator = {'init': 0,
+                 'sample_protein': [],
+                 'new_dna': [],
+                 'generate': [],
+                 'mutate': [],
+                 'mutate_protein': [],
+                 'cross': [],
+                 'get_dna': 0}
+
+simple_evolution = {'init': 0,
+                    'evolve': [],
+                    'new_generation': [],
+                    'fit': [],
+                    'shell': [],
+                    'record': [],
+                    'get_fitted_dna': [],
+                    'random_name': [],
+                    'inherit_name': [],
+                    'show_fittest': []}
+
+
+""" tools.py """
+
+grapher = {'init': [],
+           'initialize': [],
+           'live_graph': [],
+           'draw_graph': [],
+           'build': [],
+           'set-pause': []}
+
+
