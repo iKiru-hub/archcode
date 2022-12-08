3bc40ab2066559187f6674f1b93ace33337ebc95
Krubeal <dkirubel@gmail.com>
2022-12-08 13:52:48 +0100

    new folder structure

---
+++ archcode/__init__.py
@@ -0,0 +1,3 @@
+from .main import *
+from .neuromix import *
+from .archcode import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='archcode',
+   version='1.0',
+   description='a module for visualizing the structure of the neuromix module',
+   author='Krubeal Danieli',
+   author_email='dkirubel@gmail.com',
+   packages=['archcode'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
